5f1e3d1dbdf80c7df5b587791319a36d6b06099c
Jenslee Dsouza <33950242+LOCKhart07@users.noreply.github.com>
2021-07-24 14:16:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import datetime
+import json
+from discord_webhook import DiscordWebhook
+while 1:
+    t= datetime.datetime.now()
+    date=str(t.strftime("%d")+"-"+t.strftime("%m")+"-"+t.strftime("%Y"))
+
+    apiLink='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=394&date='+date
+    x = requests.get(apiLink, headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}).text
+    y = json.loads(x)
+
+    for center in y["centers"]:
+        if(center["sessions"][0]["available_capacity"])!=0 :
+            print("kara re register")
+            messageString="Center : " + center["name"] + "\ncapacity : " + str(center["sessions"][0]["available_capacity"])
+            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/841000673121927198/wd52CPsIYID1bUsVVxI5ju8qfF6zn-rgloG_DVoaqtclgNfBg8oJnqpK-CSvBOOvzayR', content=messageString)
+            response = webhook.execute()
+            print("Webhook called")
+
+    print("hi")
+    time.sleep(10)
